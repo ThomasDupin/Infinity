c2bc74198c63c737b10dfa0af403dfa8183368c3
ThomasR <thomas.rapin@viacesi.fr>
2022-03-05 14:40:17 +0100

    Modification annimation du Personnage

--- GameJam2022/Assets/Scripts/PlayerMove.cs
+++ GameJam2022/Assets/Scripts/PlayerMove.cs
@@ -6,7 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     public float speedforward = 5.0f;
     public float speedbackward = 3f;
-    public float speedrun = 12f;
+    public float speedrun = 8f;
     public float rotateSpeed = 200f;
     public float jump = 3.0f;
     private float gravityValue = -9.81f;
@@ -45,7 +45,7 @@ public class PlayerMove : MonoBehaviour
 
     private float curSpeed;
     public void MouvementForward(){
-        if(Input.GetAxis("Vertical") !=0 && Input.GetButton("Run")){
+        if(Input.GetAxis("Vertical") > 0 && Input.GetButton("Run")){
             curSpeed = speedrun * Input.GetAxis("Vertical");
         }
         else if(Input.GetAxis("Vertical") > 0){
@@ -59,11 +59,11 @@ public class PlayerMove : MonoBehaviour
         Debug.Log(curSpeed);
     }
 
-    private float smoothTime = 0.3f;
+    private float smoothTime = 0.5f;
     private float yVelocity = 0.0f;
     private float currentYRotation;
     public void Rotation(){
-        currentYRotation = Mathf.SmoothDamp(currentYRotation, Input.GetAxis("Horizontal") * rotateSpeed *Time.deltaTime, ref yVelocity, smoothTime);
-        //currentYRotation = Input.GetAxis("Horizontal") * rotateSpeed *Time.deltaTime;
+        //currentYRotation = Mathf.SmoothDamp(currentYRotation, Input.GetAxis("Horizontal") * rotateSpeed *Time.deltaTime, ref yVelocity, smoothTime);
+        currentYRotation = Input.GetAxis("Horizontal") * rotateSpeed *Time.deltaTime;
     }
 }
